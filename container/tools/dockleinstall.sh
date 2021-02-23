8d02c88cb65617b2f8b734a658b25d23597e46a6
nwiizo <syu.m.5151@gmail.com>
2021-02-23 06:46:56 +0000

    Ubuntu dockleinstall

---
+++ container/tools/dockleinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+VERSION=$(
+ curl --silent "https://api.github.com/repos/goodwithtech/dockle/releases/latest" | \
+ grep '"tag_name":' | \
+ sed -E 's/.*"v([^"]+)".*/\1/' \
+) && curl -L -o dockle.deb https://github.com/goodwithtech/dockle/releases/download/v${VERSION}/dockle_${VERSION}_Linux-64bit.deb
+sudo dpkg -i dockle.deb && rm dockle.deb
