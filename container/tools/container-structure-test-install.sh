2e4c30c0e6df06e647c0d59f944421155a795fef
nwiizo <syu.m.5151@gmail.com>
2021-02-27 12:54:31 +0000

    add many install files

---
+++ container/tools/container-structure-test-install.sh
@@ -0,0 +1,1 @@
+curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 && chmod +x container-structure-test-linux-amd64 && sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
