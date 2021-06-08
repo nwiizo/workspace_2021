784aed50ce9a8621efa8b9264fe2d86bfc27164b
nwiizo <shuya.motouchi@3-shake.com>
2021-06-08 12:08:48 +0900

    add privete ip address

--- regexp/005/main.go
+++ regexp/005/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"regexp"
 )
+
+func privateIP(ip string) (bool, error) {
+	var err error
+	private := false
+	IP := net.ParseIP(ip)
+	if IP == nil {
+		err = errors.New("Invalid IP")
+	} else {
+		_, private24BitBlock, _ := net.ParseCIDR("10.0.0.0/8")
+		_, private20BitBlock, _ := net.ParseCIDR("172.16.0.0/12")
+		_, private16BitBlock, _ := net.ParseCIDR("192.168.0.0/16")
+		private = private24BitBlock.Contains(IP) || private20BitBlock.Contains(IP) || private16BitBlock.Contains(IP)
+	}
+	return private, err
+}
 
 func main() {
 	// url の指定
@@ -24,5 +41,6 @@ func main() {
 	fmt.Println(links)
 	for i := 0; i < len(links); i++ {
 		fmt.Println(links[i])
+		fmt.Println(privateIP(links[i]))
 	}
 }
