afa8efc40b35b543bf76b066141f708cf8d8b573
Shuya Motouchi <syu.m.5151@gmail.com>
2021-11-05 10:34:47 +0900

    [add] 002

---
+++ httppath/002/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+)
+
+func main() {
+	data, _ := os.Open("urls.txt")
+
+	defer data.Close()
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		baseURL, err := url.Parse(scanner.Text())
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("URL: %s\n", baseURL.String())
+		fmt.Printf("Host: %s\n", baseURL.Host)
+		fmt.Printf("Hostname(): %s\n", baseURL.Hostname())
+		fmt.Printf("Path: %s\n", baseURL.Path)
+		fmt.Printf("RawPath: %s\n", baseURL.RawPath)
+		fmt.Printf("RawQuery: %s\n", baseURL.RawQuery)
+		fmt.Printf("Fragment: %s\n", baseURL.Fragment)
+		for key, values := range baseURL.Query() {
+			fmt.Printf("Query Key: %s\n", key)
+			for i, v := range values {
+				fmt.Printf("Query Value[%d]: %s\n", i, v)
+			}
+		}
+	}
+}
